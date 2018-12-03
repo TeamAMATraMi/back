b24c065ff8b70170f29d92551f27c3e281701a59
Steve <stevedbz@hotmail.fr>
2018-12-03 17:54:30 +0100

    ajout de modeles

---
+++ src/main/java/model/Groupe.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Groupe {
+    private int idGroupe;
+    private int idSite;
+    private String nom;
+
+    public Groupe(int idGroupe, int idSite, String nom) {
+        this.idGroupe = idGroupe;
+        this.idSite = idSite;
+        this.nom = nom;
+    }
+
+    public int getIdGroupe() { return idGroupe; }
+    public int getIdSite() { return idSite; }
+    public String getNom() { return nom; }
+
+    public void setIdGroupe(int idGroupe) {
+        this.idGroupe = idGroupe;
+    }
+
+    public void setIdSite(int idSite) {
+        this.idSite = idSite;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+
+
+}
+
