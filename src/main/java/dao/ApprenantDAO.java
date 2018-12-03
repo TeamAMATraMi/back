3f9a08872ccde7ef8702efbbda40c64601a27b58
Tarek <dalati.m.t@hotmail.fr>
2018-12-03 21:23:44 +0100

    Nettoyage du git

--- src/main/java/dao/ApprenantDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-public interface ApprenantDAO {
-    public List<Apprenant> findAll();
-    public Apprenant findById(int id);
-    public void add(Apprenant apprenant);
-    public Apprenant updateById(int id, Apprenant apprenant);
-    public void deleteById(int id);
-}
--- src/main/java/dao/GroupeDAO.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-public interface GroupeDAO {
-    public List<Groupe> findAll();
-    public Groupe findById(int id);
-    public void add(Groupe groupe);
-    public Groupe updateById(int id, Groupe groupe);
-    public void deleteById(int id);
-}
