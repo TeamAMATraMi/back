1768931799a7e0ca05f845b2bfab9d4e02cf01b5
Tarek <dalati.m.t@hotmail.fr>
2018-12-06 18:17:41 +0100

    Modification du DAO et du modèle Sejour

--- src/main/java/application/dao/PaysDAOimpl.java
+++ src/main/java/application/dao/PaysDAOimpl.java
@@ -1,4 +1,51 @@
 package application.dao;
 
-public class PaysDAOimpl {
+import application.model.Pays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaysDAOimpl implements PaysDAO {
+
+    private List<Pays> listePays = new ArrayList<>();
+
+    @Override
+    public List<Pays> findAll() {
+        return this.listePays;
+    }
+
+    @Override
+    public Pays findById(int id) {
+        for (Pays a : this.listePays) {
+            if (a.getId() == id) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void add(Pays pays) {
+        this.listePays.add(pays);
+    }
+
+    @Override
+    public Pays updateById(int id, Pays pays) {
+        for (int i = 0; i < this.listePays.size(); i++) {
+            if (this.listePays.get(i).getId() == id) {
+                this.listePays.set(i, pays);
+                return this.listePays.get(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        for (int i = 0; i < this.listePays.size(); i++) {
+            if (this.listePays.get(i).getId() == id) {
+                this.listePays.remove(i);
+            }
+        }
+    }
 }
--- src/main/java/application/dao/PresenceDAOimpl.java
+++ src/main/java/application/dao/PresenceDAOimpl.java
@@ -1,4 +1,51 @@
 package application.dao;
 
-public class PresenceDAOimpl {
+import application.model.Presence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PresenceDAOimpl implements PresenceDAO {
+
+    private List<Presence> listePresence = new ArrayList<>();
+
+    @Override
+    public List<Presence> findAll() {
+        return this.listePresence;
+    }
+
+    @Override
+    public Presence findById(int id) {
+        for (Presence a : this.listePresence) {
+            if (a.getId() == id) {
+                return a;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void add(Presence presence) {
+        this.listePresence.add(presence);
+    }
+
+    @Override
+    public Presence updateById(int id, Presence presence) {
+        for (int i = 0; i < this.listePresence.size(); i++) {
+            if (this.listePresence.get(i).getId() == id) {
+                this.listePresence.set(i, presence);
+                return this.listePresence.get(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        for (int i = 0; i < this.listePresence.size(); i++) {
+            if (this.listePresence.get(i).getId() == id) {
+                this.listePresence.remove(i);
+            }
+        }
+    }
 }
