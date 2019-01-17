4809a15a6893bb33e86161c994374df9c21c0c8a
Frodonche <Frodonche@github.com>
2019-01-17 16:35:21 +0100

    Changements champs_underscore par ChampsUnderscore

--- src/main/java/application/model/Apprenant.java
+++ src/main/java/application/model/Apprenant.java
@@ -15,30 +15,30 @@ public class Apprenant {
   private String prenom;
   private String telephone;
   private String adresse;
-  private int code_postal;
+  private String codePostal;
   private String commune;
-  private int id_groupe;
-  private String date_inscription; // a parser in/out
-  private int auteur_dossier;
+  private int idGroupe;
+  private String dateInscription; // a parser in/out
+  private int auteurDossier;
   private int majeur;
-  private String date_naissance; // a parser in/out
+  private String dateNaissance; // a parser in/out
   private char genre;
-  private String pays_origine;
+  private String paysOrigine;
   private String nationalite;
-  private String date_arrivee; // a parser in/out
-  private int quartier_prioritaire; // clef étrangère
-  private String situation_personelle;
-  private int prise_charge;
+  private String dateArrivee; // a parser in/out
+  private int quartierPrioritaire; // clef étrangère
+  private String situationPersonelle;
+  private int priseCharge;
   private boolean rsa;
-  private int temps_scolarisation;
+  private int tempsScolarisation;
   private String diplome;
-  private boolean milieu_scolaire;
-  private String niveau_langue;
-  private boolean lire_langue;
-  private boolean ecrire_langue;
-  private boolean lire_alpha_latin;
-  private boolean ecrire_alpha_latin;
-  private boolean cotisation_payee;
+  private boolean milieuScolaire;
+  private String niveauLangue;
+  private boolean lireLangue;
+  private boolean ecrireLangue;
+  private boolean lireAlphaLatin;
+  private boolean ecrireAlphaLatin;
+  private boolean cotisationPayee;
   private String remarques;
 
   public Apprenant(){
@@ -51,215 +51,301 @@ public class Apprenant {
            String prenom,
            String telephone,
            String adresse,
-           int code_postal,
+           String codePostal,
            String commune,
-           int id_groupe,
-           String date_inscription,
-           int auteur_dossier,
+           int idGroupe,
+           String dateInscription,
+           int auteurDossier,
            int majeur,
-           String date_naissance,
+           String dateNaissance,
            char genre,
-           String pays_origine,
+           String paysOrigine,
            String nationalite,
-           String date_arrivee,
-           int quartier_prioritaire,
-           String situation_personelle,
-           int prise_charge,
+           String dateArrivee,
+           int quartierPrioritaire,
+           String situationPersonelle,
+           int priseCharge,
            boolean rsa,
-           int temps_scolarisation,
+           int tempsScolarisation,
            String diplome,
-           boolean milieu_scolaire,
-           String niveau_langue,
-           boolean lire_langue,
-           boolean ecrire_langue,
-           boolean lire_alpha_latin,
-           boolean ecrire_alpha_latin,
-           boolean cotisation_payee,
-           String remarques){
+           boolean milieuScolaire,
+           String niveauLangue,
+           boolean lireLangue,
+           boolean ecrireLangue,
+           boolean lireAlphaLatin,
+           boolean ecrireAlphaLatin,
+           boolean cotisationPayee,
+           String remarques) {
 
       this.id = id;
       this.nom = nom;
       this.prenom = prenom;
       this.telephone = telephone;
       this.adresse = adresse;
-      this.code_postal = code_postal;
+      this.codePostal = codePostal;
       this.commune = commune;
-      this.id_groupe = id_groupe;
-      this.date_inscription = date_inscription;
-      this.auteur_dossier = auteur_dossier;
+      this.idGroupe = idGroupe;
+      this.dateInscription = dateInscription;
+      this.auteurDossier = auteurDossier;
       this.majeur = majeur;
-      this.date_naissance = date_naissance;
+      this.dateNaissance = dateNaissance;
       this.genre = genre;
-      this.pays_origine = pays_origine;
+      this.paysOrigine = paysOrigine;
       this.nationalite = nationalite;
-      this.date_arrivee = date_arrivee;
-      this.quartier_prioritaire = quartier_prioritaire;
-      this.situation_personelle = situation_personelle;
-      this.prise_charge = prise_charge;
+      this.dateArrivee = dateArrivee;
+      this.quartierPrioritaire = quartierPrioritaire;
+      this.situationPersonelle = situationPersonelle;
+      this.priseCharge = priseCharge;
       this.rsa = rsa;
-      this.temps_scolarisation = temps_scolarisation;
+      this.tempsScolarisation = tempsScolarisation;
       this.diplome = diplome;
-      this.milieu_scolaire = milieu_scolaire;
-      this.niveau_langue = niveau_langue;
-      this.lire_langue = lire_langue;
-      this.ecrire_langue = ecrire_langue;
-      this.lire_alpha_latin = lire_alpha_latin;
-      this.ecrire_alpha_latin = ecrire_alpha_latin;
-      this.cotisation_payee = cotisation_payee;
+      this.milieuScolaire = milieuScolaire;
+      this.niveauLangue = niveauLangue;
+      this.lireLangue = lireLangue;
+      this.ecrireLangue = ecrireLangue;
+      this.lireAlphaLatin = lireAlphaLatin;
+      this.ecrireAlphaLatin = ecrireAlphaLatin;
+      this.cotisationPayee = cotisationPayee;
       this.remarques = remarques;
   }
 
-    /* ----------------------------PARTIE DES GETTERS ------------------------------*/
+    public int getId() {
+        return id;
+    }
 
-    public int getId() { return id; }
-    public String getNom() { return nom; }
-    public String getPrenom() { return prenom; }
-    public String getTelephone() { return telephone; }
-    public String getAdresse() { return adresse; }
-    public int getCodePostal() { return code_postal; }
-    public String getCommune() { return commune; }
-    public int getIdGroupe() { return id_groupe; }
-    public String getDateInscription() { return date_inscription; }
-    public int getAuteurDossier() { return auteur_dossier; }
-    public int getMajeur() { return majeur; }
-    public String getDateNaissance() { return date_naissance; }
-    public char getGenre() { return genre; }
-    public String getPaysOrigine() { return pays_origine; }
-    public String getNationalite() { return nationalite; }
-    public String getDateArrivee() { return date_arrivee; }
-    public int getQuartierPrioritaire() { return quartier_prioritaire; }
-    public String getSituationPersonelle() { return situation_personelle; }
-    public int getPriseCharge() { return prise_charge; }
-    public boolean isRsa() { return rsa; }
-    public int getTempsScolarisation() { return temps_scolarisation; }
-    public String getDiplome() { return diplome; }
-    public boolean isMilieuScolaire() { return milieu_scolaire; }
-    public String getNiveauLangue() { return niveau_langue; }
-    public boolean isLireLangue() { return lire_langue; }
-    public boolean isEcrireLangue() { return ecrire_langue; }
-    public boolean isLireAlphaLatin() { return lire_alpha_latin; }
-    public boolean isEcrireAlphaLatin() { return ecrire_alpha_latin; }
-    public boolean isCotisationPayee() { return cotisation_payee; }
-    public String getRemarques() { return remarques; }
-    /* ---------------------- FIN DES GETTERS --------------------------------*/
+    public void setId(int id) {
+        this.id = id;
+    }
 
-    /* ---------------------- PARTIE DES SETTERS ---------------------------- */
+    public String getNom() {
+        return nom;
+    }
+
     public void setNom(String nom) {
         this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
     }
 
     public void setPrenom(String prenom) {
         this.prenom = prenom;
     }
 
+    public String getTelephone() {
+        return telephone;
+    }
+
     public void setTelephone(String telephone) {
         this.telephone = telephone;
+    }
+
+    public String getAdresse() {
+        return adresse;
     }
 
     public void setAdresse(String adresse) {
         this.adresse = adresse;
     }
 
-    public void setCode_postal(int code_postal) {
-        this.code_postal = code_postal;
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+
+    public String getCommune() {
+        return commune;
     }
 
     public void setCommune(String commune) {
         this.commune = commune;
     }
 
-    public void setId_groupe(int id_groupe) {
-        this.id_groupe = id_groupe;
+    public int getIdGroupe() {
+        return idGroupe;
     }
 
-    public void setDate_inscription(String date_inscription) {
-        this.date_inscription = date_inscription;
+    public void setIdGroupe(int idGroupe) {
+        this.idGroupe = idGroupe;
     }
 
-    public void setAuteur_dossier(int auteur_dossier) {
-        this.auteur_dossier = auteur_dossier;
+    public String getDateInscription() {
+        return dateInscription;
+    }
+
+    public void setDateInscription(String dateInscription) {
+        this.dateInscription = dateInscription;
+    }
+
+    public int getAuteurDossier() {
+        return auteurDossier;
+    }
+
+    public void setAuteurDossier(int auteurDossier) {
+        this.auteurDossier = auteurDossier;
+    }
+
+    public int getMajeur() {
+        return majeur;
     }
 
     public void setMajeur(int majeur) {
         this.majeur = majeur;
     }
 
-    public void setDate_naissance(String date_naissance) {
-        this.date_naissance = date_naissance;
+    public String getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(String dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public char getGenre() {
+        return genre;
     }
 
     public void setGenre(char genre) {
         this.genre = genre;
     }
 
-    public void setPays_origine(String pays_origine) {
-        this.pays_origine = pays_origine;
+    public String getPaysOrigine() {
+        return paysOrigine;
+    }
+
+    public void setPaysOrigine(String paysOrigine) {
+        this.paysOrigine = paysOrigine;
+    }
+
+    public String getNationalite() {
+        return nationalite;
     }
 
     public void setNationalite(String nationalite) {
         this.nationalite = nationalite;
     }
 
-    public void setDate_arrivee(String date_arrivee) {
-        this.date_arrivee = date_arrivee;
+    public String getDateArrivee() {
+        return dateArrivee;
     }
 
-    public void setQuartier_prioritaire(int quartier_prioritaire) {
-        this.quartier_prioritaire = quartier_prioritaire;
+    public void setDateArrivee(String dateArrivee) {
+        this.dateArrivee = dateArrivee;
     }
 
-    public void setSituation_personelle(String situation_personelle) {
-        this.situation_personelle = situation_personelle;
+    public int getQuartierPrioritaire() {
+        return quartierPrioritaire;
     }
 
-    public void setPrise_charge(int prise_charge) {
-        this.prise_charge = prise_charge;
+    public void setQuartierPrioritaire(int quartierPrioritaire) {
+        this.quartierPrioritaire = quartierPrioritaire;
+    }
+
+    public String getSituationPersonelle() {
+        return situationPersonelle;
+    }
+
+    public void setSituationPersonelle(String situationPersonelle) {
+        this.situationPersonelle = situationPersonelle;
+    }
+
+    public int getPriseCharge() {
+        return priseCharge;
+    }
+
+    public void setPriseCharge(int priseCharge) {
+        this.priseCharge = priseCharge;
+    }
+
+    public boolean isRsa() {
+        return rsa;
     }
 
     public void setRsa(boolean rsa) {
         this.rsa = rsa;
     }
 
-    public void setTemps_scolarisation(int temps_scolarisation) {
-        this.temps_scolarisation = temps_scolarisation;
+    public int getTempsScolarisation() {
+        return tempsScolarisation;
+    }
+
+    public void setTempsScolarisation(int tempsScolarisation) {
+        this.tempsScolarisation = tempsScolarisation;
+    }
+
+    public String getDiplome() {
+        return diplome;
     }
 
     public void setDiplome(String diplome) {
         this.diplome = diplome;
     }
 
-    public void setMilieu_scolaire(boolean milieu_scolaire) {
-        this.milieu_scolaire = milieu_scolaire;
+    public boolean isMilieuScolaire() {
+        return milieuScolaire;
     }
 
-    public void setNiveau_langue(String niveau_langue) {
-        this.niveau_langue = niveau_langue;
+    public void setMilieuScolaire(boolean milieuScolaire) {
+        this.milieuScolaire = milieuScolaire;
     }
 
-    public void setLire_langue(boolean lire_langue) {
-        this.lire_langue = lire_langue;
+    public String getNiveauLangue() {
+        return niveauLangue;
     }
 
-    public void setEcrire_langue(boolean ecrire_langue) {
-        this.ecrire_langue = ecrire_langue;
+    public void setNiveauLangue(String niveauLangue) {
+        this.niveauLangue = niveauLangue;
     }
 
-    public void setLire_alpha_latin(boolean lire_alpha_latin) {
-        this.lire_alpha_latin = lire_alpha_latin;
+    public boolean isLireLangue() {
+        return lireLangue;
     }
 
-    public void setEcrire_alpha_latin(boolean ecrire_alpha_latin) {
-        this.ecrire_alpha_latin = ecrire_alpha_latin;
+    public void setLireLangue(boolean lireLangue) {
+        this.lireLangue = lireLangue;
     }
 
-    public void setCotisation_payee(boolean cotisation_payee) {
-        this.cotisation_payee = cotisation_payee;
+    public boolean isEcrireLangue() {
+        return ecrireLangue;
+    }
+
+    public void setEcrireLangue(boolean ecrireLangue) {
+        this.ecrireLangue = ecrireLangue;
+    }
+
+    public boolean isLireAlphaLatin() {
+        return lireAlphaLatin;
+    }
+
+    public void setLireAlphaLatin(boolean lireAlphaLatin) {
+        this.lireAlphaLatin = lireAlphaLatin;
+    }
+
+    public boolean isEcrireAlphaLatin() {
+        return ecrireAlphaLatin;
+    }
+
+    public void setEcrireAlphaLatin(boolean ecrireAlphaLatin) {
+        this.ecrireAlphaLatin = ecrireAlphaLatin;
+    }
+
+    public boolean isCotisationPayee() {
+        return cotisationPayee;
+    }
+
+    public void setCotisationPayee(boolean cotisationPayee) {
+        this.cotisationPayee = cotisationPayee;
+    }
+
+    public String getRemarques() {
+        return remarques;
     }
 
     public void setRemarques(String remarques) {
         this.remarques = remarques;
     }
-
-    /* ---------------------- FIN DES SETTERS ----------------------- */
-
 }
