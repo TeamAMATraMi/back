e93165577b7f4946150d82e285a4f4d39b5802e3
Frodonche <Frodonche@github.com>
2019-01-18 19:09:10 +0100

    LE DELETE FONCTIONNE git add src/*

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -34,8 +34,8 @@ public class Application implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedOrigins("*").allowedHeaders("*");
-        registry.addMapping("/*/*").allowedOrigins("*").allowedHeaders("*");
+        registry.addMapping("/*").allowedOrigins("*").allowedHeaders("*").allowedMethods("GET", "POST","PUT", "DELETE");
+        registry.addMapping("/*/*").allowedOrigins("*").allowedHeaders("*").allowedMethods("GET", "POST","PUT", "DELETE");
     }
 
 }
