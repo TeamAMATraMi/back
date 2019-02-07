0233d90104c77f137082f38f9e64707105d01536
FrLallemand <frlallemand@protonmail.com>
2019-02-07 16:44:01 +0100

    mkljhgf

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /deploy/applitrami-back/
 docker-compose up -d
-kill $(jps -v | grep AppliTrami-0.1.0.jar | grep -o '^[0-9]*')
+#kill $(jps -v | grep AppliTrami-0.1.0.jar | grep -o '^[0-9]*')
 java -jar target/AppliTrami-0.1.0.jar
