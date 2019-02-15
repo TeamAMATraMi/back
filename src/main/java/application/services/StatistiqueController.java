cc743d2cdf3564f3a6ef37397fc62c4b649f47a5
Laura-Weber <laura.weber57@laposte.net>
2019-02-15 14:06:34 +0100

    debut bis des statistiques

--- src/main/java/application/services/StatistiqueController.java
+++ src/main/java/application/services/StatistiqueController.java
@@ -16,7 +16,7 @@ public class StatistiqueController {
     @Autowired
     private ApprenantDAO apprenantDAO;
 
-    @GetMapping("/statistiques/sexe")
+    @GetMapping("/sexe")
     public Map<String, Integer> getBySexe() {
         Map<String, Integer> res = new HashMap<>();
         int cmpF = 0;
@@ -27,10 +27,40 @@ public class StatistiqueController {
                 cmpF++;
             }
         }
-
         res.put("F", cmpF);
-        res.put("F", (tmp.size() - cmpF));
+        res.put("M", (tmp.size() - cmpF));
+        return res;
+    }
+
+    @GetMapping("/nationalite")
+    public Map<String, Integer> getByNationalite() {
+        Map<String, Integer> res = new HashMap<>();
+        List<Apprenant> tmp = this.apprenantDAO.findAll();
+        for(Apprenant a : tmp){
+            if(res.containsKey(a.getPaysOrigine())){
+                res.put(a.getPaysOrigine(), res.get(a.getPaysOrigine()) + 1);
+            }else{
+                res.put(a.getPaysOrigine(), 1);
+            }
+        }
+        return res;
+    }
+
+    @GetMapping("/age")
+    public Map<String, Integer> getByAge() {
+        Map<String, Integer> res = new HashMap<>();
+        List<Apprenant> tmp = this.apprenantDAO.findAll();
+
+        for(Apprenant a : tmp){
+            String[] stmp = a.getDateNaissance().split("-");
+            if(res.containsKey(stmp[0])){
+                res.put(stmp[0], res.get(stmp[0]) + 1);
+            }else{
+                res.put(stmp[0], 1);
+            }
+        }
 
         return res;
     }
+
 }
